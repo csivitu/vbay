cc78d9be9dd96c9d9cbef231118154dd69d3f5f8
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-10-02 11:16:49 +0530

    fix: darkmode index page

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 function darkModeFunction() {
     var element = document.body;
+    var listElement = document.getElementById('element');
+    var darkmode_class_name = "darkMode";
+    listElement.classList.toggle(darkmode_class_name);
     element.classList.toggle("darkMode");
  }
