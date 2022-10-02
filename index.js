be99329d6c75de70d67311e156a077ceedcba5e7
Gaurav Hegde <gaurav.hegde2021@vitstudent.ac.in>
2022-10-02 09:05:13 +0530

    hamburger menu

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ function darkModeFunction() {
     var element = document.body;
     element.classList.toggle("darkMode");
  }
+
+ function onMenuClick() {
+    var navbar = document.getElementById('navigation-bar');
+    var responsive_class_name = 'responsive'
+    navbar.classList.toggle(responsive_class_name)
+}
